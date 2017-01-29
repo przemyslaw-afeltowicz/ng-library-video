f6ebfd1e2ee69a95fb20fcfcec8d5ff103d41415
veranoo <przemyslaw.afeltowicz@gmail.com>
2017-01-29 23:16:03 +0100

    fixed service

--- src/app/films.service.ts
+++ src/app/films.service.ts
@@ -6,10 +6,10 @@ export class FilmsService {
   films = [
     {
       id: 1,
-      thumb: "../../assets/films/thumb.jpg",
+      thumb: "http://news.mit.edu/sites/mit.edu.newsoffice/files/images/2016/MIT-Earth-Dish_0.jpg",
       title: "Tytuł filmu",
       description: "Opis filmu",
-      source: "../../assets/films/RoadTrip.mp4",
+      source: "http://projekty.pro-comp.pl/nowy/video.mp4",
       category: "Komedia",
       director: "Adam Kowalski",
       actors: []
@@ -17,21 +17,21 @@ export class FilmsService {
     {
       id: 2,
       title: "Lorem ipsum",
-      thumb: "../../assets/films/thumb.jpg",
+      thumb: "http://cssslider.com/sliders/demo-5/data1/images/road_forest_trees_tree_trip_nature_leaves_season.jpg",
       description: "Opis filmu",
-      source: "../../assets/films/honey.mp4",
-      category: "Komedia",
-      director: "Adam Kowalski",
+      source: "http://projekty.pro-comp.pl/nowy/video.mp4",
+      category: "Dramat",
+      director: "Piotr Nowak",
       actors: []
     },
     {
       id: 3,
-      title: "Lorem ipsum",
-      thumb: "../../assets/films/thumb.jpg",
+      title: "Sintel",
+      thumb: "https://vodo.net/media/screenshots/work_138.jpeg",
       description: "Opis filmu",
       source: "https://media.w3.org/2010/05/sintel/trailer.mp4",
       category: "Komedia",
-      director: "Adam Kowalski",
+      director: "Przemysław Testowy",
       actors: []
     }
   ];
