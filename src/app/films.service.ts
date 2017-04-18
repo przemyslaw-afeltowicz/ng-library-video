5c21b09ceab5fd879078360d776b6ececcbccf4d
Przemysław Afeltowicz <pafeltowicz@cyfrowypolsat.pl>
2017-04-18 16:56:26 +0200

    poprawki zbiorcze

--- src/app/films.service.ts
+++ src/app/films.service.ts
@@ -1,4 +1,7 @@
 import {Injectable} from '@angular/core';
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/toPromise';
+import {Observable} from "rxjs";
 
 @Injectable()
 export class FilmsService {
@@ -6,40 +9,40 @@ export class FilmsService {
   films = [
     {
       id: 1,
-      thumb: "https://news.mit.edu/sites/mit.edu.newsoffice/files/images/2016/MIT-Earth-Dish_0.jpg",
-      title: "Tytuł filmu",
-      description: "Opis filmu",
-      source: "http://projekty.pro-comp.pl/nowy/video.mp4",
+      thumb: "http://s.redefine.pl/dcs/o2/redefine/vm2images/xu/xuv1rcqi7knjje5rsee3a8j4y3zm2oe5.jpg",
+      title: "10-lecie Kabaretu Skeczów Męczących - Część II",
+      description: "Dla czcigodnego jubilata wystąpią zespoły Feel i Enej, kabaret Łowcy.B i Andrzej Grabows...",
+      source: "http://n-3-8.dcs.redcdn.pl/webcache/rdf2/redefine/p/vm2movies/ir/irgujr89gvxojpsq5pf5s2szd5rcmy2j.mp4",
       category: "Komedia",
       director: "Adam Kowalski",
       actors: []
     },
     {
       id: 2,
-      title: "Lorem ipsum",
-      thumb: "https://static1.squarespace.com/static/54406c8fe4b0b8e2e81ba876/58351244e3df28974c9282cf/5835127a579fb34d13e304a3/1479873150361/pexels-photo-196667.jpeg",
-      description: "Opis filmu",
-      source: "http://projekty.pro-comp.pl/nowy/video.mp4",
+      title: "Świat według Kiepskich - Odcinek 511",
+      thumb: "http://s.redefine.pl/dcs/o2/redefine/vm2images/a3/a3gr6hu7a8isn543k8ttj41g4ufr7c2n.jpg",
+      description: "Halina przypadkowo znajduje w piwnicy stary album rodzinny...",
+      source: "http://n-3-20.dcs.redcdn.pl/webcache/rdf1/redefine/p/vm2movies/xf/xfahf2b2uqrbkzm122pmmicppgimnfcp.mp4",
       category: "Dramat",
       director: "Piotr Nowak",
       actors: []
     },
     {
       id: 3,
-      title: "Sintel",
-      thumb: "https://vodo.net/media/screenshots/work_138.jpeg",
-      description: "Opis filmu",
-      source: "https://media.w3.org/2010/05/sintel/trailer.mp4",
+      title: "Idol - Odcinek 9",
+      thumb: "http://s.redefine.pl/dcs/o2/redefine/vm2images/gn/gnywtqzp981f2ky3tkmw3yoy8k5jabx2.jpg",
+      description: "Zaczęło się eliminowanie finalistów „Idola”. Pierwszy uczestnik odpadł po występie na żywo. Tym razem to widzowie decydowali o „być albo nie być” kandydatów na gwiazdy...",
+      source: "http://n-3-23.dcs.redcdn.pl/webcache/rdf1/redefine/p/vm2movies/g4/g47822ukch3dewyqf4gqftp2ch5uq5v7.mp4",
       category: "Komedia",
       director: "Przemysław Testowy",
       actors: []
     },
     {
       id: 4,
-      title: "Paluch",
-      thumb: "https://vodo.net/media/screenshots/work_138.jpeg",
-      description: "Paluch - Gdy kiedyś....",
-      source: "https://r2---sn-f5f7ln7s.googlevideo.com/videoplayback?pl=18&mime=video/mp4&ei=LNqVWL3JCpGvWO2iv-AN&clen=13843438&expire=1486237324&id=o-AOkkjsFfJJ3cloaokaNyqHQ7NkVQxIZnFPiS0xPo0lCo&itag=18&sparams=clen,dur,ei,expire,gir,id,initcwndbps,ip,ipbits,ipbypass,itag,lmt,mime,mm,mn,ms,mv,nh,pl,ratebypass,requiressl,source,upn&upn=QxCWdLSXjZ4&gir=yes&requiressl=yes&ipbits=0&ip=77.91.32.28&lmt=1480018396873715&dur=234.057&ratebypass=yes&source=youtube&key=cms1&signature=4E92E35F39D316736DA80FC167E2430CEAEE6F1F.7DD7DECE95E23FC2A8A705C41E8DC6851C6BE46D&title=WWW.DOWNVIDS.NET-Paluch%20%22Gdybys%CC%81%20kiedys%CC%81%22%20prod.%20Maiky%20Beatz%20(%20OFFICIAL%20VIDEO%20)&redirect_counter=1&req_id=6452f4cc1e15a3ee&cms_redirect=yes&ipbypass=yes&mm=31&mn=sn-f5f7ln7s&ms=au&mt=1486215679&mv=m&nh=IgpwcjAyLndhdzAyKgkxMjcuMC4wLjE",
+      title: "Zdrady - Odcinek 106",
+      thumb: "http://s.redefine.pl/dcs/o2/redefine/vm2images/ge/gehhfaz538msgocvgceywa444vbc1oip.jpg",
+      description: "Marek Kuchciński (29 l.) ożenił się z Elizą (27 l.) sześć tygodni temu. Mimo iż od dnia ślubu minęło już trochę czasu, ona wciąż nie zachowuje...",
+      source: "http://n-3-5.dcs.redcdn.pl/webcache/rdf2/redefine/p/vm2movies/bj/bjkwyjueh9c372dbe6n1ngekxihj3q2g.mp4",
       category: "Polish rap",
       director: "Paluch",
       actors: []
@@ -47,11 +50,14 @@ export class FilmsService {
 
   ];
 
-  constructor() {
-  }
+  constructor(private http: Http) {}
 
   getFilms() {
     return this.films;
+  }
+
+  getArtist(){
+    return this.http.get('https://api.spotify.com/v1/search?q=eminem&type=artist');
   }
 
   getFilm(id) {
